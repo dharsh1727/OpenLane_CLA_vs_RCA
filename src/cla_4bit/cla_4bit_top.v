71db27b11fe28d6fb920ab86d84687e1cc2790a3
dharsh1727 <dharsh1727s@gmail.com>
2026-04-26 16:30:18 +0530

    Add files via upload

---
+++ src/cla_4bit/cla_4bit_top.v
@@ -0,0 +1,27 @@
+module cla_4bit_top(
+    input        clk,
+    input  [3:0] A,
+    input  [3:0] B,
+    input        Cin,
+    output reg [3:0] Sum,
+    output reg       Cout
+);
+
+    wire [3:0] sum_w;
+    wire       cout_w;
+
+    cla_4bit u_cla (
+        .A(A),
+        .B(B),
+        .Cin(Cin),
+        .Sum(sum_w),
+        .Cout(cout_w)
+    );
+
+    always @(posedge clk) begin
+        Sum  <= sum_w;
+        Cout <= cout_w;
+    end
+
+endmodule
+
